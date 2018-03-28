c7ebd8fbdce780dab459617ef51acedf658e28e4
Sanjana <sanjana11147@gmail.com>
2018-03-28 23:59:18 +0530

    Added comments

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from snippets.forms import SnippetForm
+from django.http import HttpResponse, Http404
+from snippets.forms import SnippetForm, CommentForm
 from snippets.models import Snippet
 
 def create(request):
@@ -16,4 +16,11 @@ def create(request):
 	return render(request, 'snippets/create.html', {'form': form})
 
 def view(request, key):
-	return HttpResponse("View Function: " + str(key))
+	if request.method == "GET":
+		if Snippet.objects.filter(id=key).exists():
+			snippet = Snippet.objects.get(id=key)
+			form = CommentForm()
+			comments = snippet.comment_set.all()
+		else:
+			raise Http404("Snippet doesn't exist")
+		return render(request, 'snippets/view.html', {'key': key, 'title': snippet.title, 'content': snippet.content, 'form': form, 'comments': comments})
