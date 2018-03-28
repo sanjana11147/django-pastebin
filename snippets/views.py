c6292bb28f1a820f5d89b96268a476272afaf950
Sanjana <sanjana11147@gmail.com>
2018-03-29 00:14:34 +0530

    Pastebin completed

--- snippets/views.py
+++ snippets/views.py
@@ -16,11 +16,18 @@ def create(request):
 	return render(request, 'snippets/create.html', {'form': form})
 
 def view(request, key):
-	if request.method == "GET":
-		if Snippet.objects.filter(id=key).exists():
-			snippet = Snippet.objects.get(id=key)
-			form = CommentForm()
-			comments = snippet.comment_set.all()
-		else:
-			raise Http404("Snippet doesn't exist")
+	if Snippet.objects.filter(id=key).exists():
+		snippet = Snippet.objects.get(id=key)
+		form = CommentForm()
+		comments = snippet.comment_set.all()
+	else:
+		raise Http404("Snippet doesn't exist")
+	if request.method == "POST":
+		form = CommentForm(request.POST)
+		if form.is_valid():
+			comment = form.save(commit=False)
+			comment.snippet = snippet
+			comment.save()
+		form = CommentForm()
+		comments = snippet.comment_set.order_by('-datetime_created')
 		return render(request, 'snippets/view.html', {'key': key, 'title': snippet.title, 'content': snippet.content, 'form': form, 'comments': comments})
